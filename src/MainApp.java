cd0bfaa5933a3fea2e3b934ba40c0e12b8ebf98c
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-11 19:27:51 +0200

    Dünya değişti.

---
+++ src/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * Başlangıç sınıfıdır.
+ * @link
+ * @see
+ */
+public class MainApp {
+    /**
+     *Programın başlangıç metodudur.
+     * @param args
+     */
+    public static void main(String[] args) {
+//Ekrana mesaj yazar
+        System.out.println("Merhaba..");
+        System.out.println("Dünya");
+
+
+
+
+    }
+}
